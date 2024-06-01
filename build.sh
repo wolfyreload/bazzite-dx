24c0f373afd9079fd45f3f7d3593a39f00ef2c2f
wolfyreload <open.chords.app@gmail.com>
2024-06-01 09:41:16 +0200

    chore: add vscode

--- build.sh
+++ build.sh
@@ -12,12 +12,13 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+# this adds the vscode repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# Install additional packages
+rpm-ostree install code
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
