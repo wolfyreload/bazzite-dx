8bd3519db4471acf306482ad9301a7c3a7e85ede
wolfyreload <open.chords.app@gmail.com>
2024-06-09 16:24:22 +0200

    Revert "chore: switch virtual machine to VirtualBox"
    This reverts commit 81f1b3d8320cc578330ca6754a83638bec98ab4b.

--- build-vm.sh
+++ build-vm.sh
@@ -5,6 +5,8 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Install virtual machine software
-wget -O /tmp/virtualbox.rpm https://download.virtualbox.org/virtualbox/7.0.18/VirtualBox-7.0-7.0.18_162988_fedora40-1.x86_64.rpm
-rpm-ostree install /tmp/virtualbox.rpm
+rpm-ostree install \
+        virt-manager \
+        edk2-ovmf \
+        qemu
 
