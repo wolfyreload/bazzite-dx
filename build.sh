7465866d60d3596178dc4796d0f669caa66b4344
wolfyreload <open.chords.app@gmail.com>
2024-06-14 19:57:27 +0200

    chore: add nvidia 555 driver

--- build.sh
+++ build.sh
@@ -33,6 +33,16 @@ rpm-ostree install docker-ce \
         docker-compose-plugin \
         containerd.io
         
+# Enable nvidia driver
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_sentry-kernel-fsync.repo
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo 
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo
+rpm-ostree install nvidia-driver nvidia-driver-libs nvidia-libXNVCtrl nvidia-kmod-common nvidia-modprobe nvidia-settings nvidia-xconfig akmod-nvidia nvidia-driver-cuda nvidia-driver-cuda-libs
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_sentry-kernel-fsync.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo 
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo
+rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
+
 #### Example for enabling a System Unit File
 
 # systemctl enable podman.socket
